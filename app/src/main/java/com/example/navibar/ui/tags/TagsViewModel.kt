9996e6c5babc2acb0eb2b573091616a54a181a45
annazdancuka <anna.zdan.uka@accenture.com>
2024-07-19 11:42:20 +0300

    Little text changes

--- app/src/main/java/com/example/navibar/ui/tags/TagsViewModel.kt
+++ app/src/main/java/com/example/navibar/ui/tags/TagsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class TagsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is tags Fragment"
+        value = "This is Tags fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/navibar/ui/upload/UploadViewModel.kt
+++ app/src/main/java/com/example/navibar/ui/upload/UploadViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class UploadViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is upload Fragment"
+        value = "This is Save fragment"
     }
     val text: LiveData<String> = _text
 }
