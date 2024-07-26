c2a15aaae5c3f80b4b580906998da3d9c477e1ad
annazdancuka <anna.zdan.uka@accenture.com>
2024-07-26 09:29:16 +0300

    Librari fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,79 +58,55 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx.v190)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation(libs.room.runtime)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.navigation.fragment.ktx.v260)
-    implementation(libs.androidx.navigation.ui.ktx.v260)
-    implementation(libs.androidx.core.ktx.v1100)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx.v261)
-    implementation(libs.androidx.ui.desktop)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core.v351)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+
+    // UI
     implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.androidx.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx.v2770)
-    //implementation(libs.androidx.core.splashscreen)
-    implementation("androidx.core:core-splashscreen:1.0.1")
 
-    val room_version = "2.6.1"
+    implementation(libs.androidx.core.splashscreen)
+
     // Room components
-    implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
+    implementation(libs.room.runtime)
+    //annotationProcessor(libs.androidx.room.compiler)
+    //kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.common)
 
     // To use Kotlin annotation processing tool (kapt)
-    kapt("androidx.room:room-compiler:$room_version")
+
     // To use Kotlin Symbol Processing (KSP)
 //    ksp("androidx.room:room-compiler:$room_version")
 
     // Optional - Kotlin Extensions and Coroutines support for Room
     implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
 
     // AndroidX core components
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.fragment.ktx)
 
     // Lifecycle components
-    implementation (libs.androidx.lifecycle.livedata.ktx)
-    implementation (libs.androidx.lifecycle.viewmodel.ktx)
-
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.room.common)
-    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
     // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
 
     // Navigation components
     implementation (libs.navigation.fragment.ktx.v270)
-    implementation (libs.androidx.navigation.ui.ktx.v270)
+    implementation (libs.androidx.navigation.fragment.ktx)
+    implementation (libs.androidx.navigation.ui.ktx)
+
 
 }
 
